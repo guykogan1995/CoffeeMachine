0af82d91fd1d7dbb11909a38ffce002dbd140f72
Guy Kogan <guykogan@u.boisestate.edu>
2023-05-10 18:19:32 -0600

    Added default sort

--- main.go
+++ main.go
@@ -7,19 +7,21 @@ import (
 
 func main() {
 	AllOrders := OrdersManipulation.GetOrders()
-	//UnFulfilledOrders := OrdersManipulation.GetUnFulfilledOrders()
-	//FulfilledOrders := OrdersManipulation.GetFulfilledOrders()
+	UnFulfilledOrders := OrdersManipulation.GetUnFulfilledOrders()
+	FulfilledOrders := OrdersManipulation.GetFulfilledOrders()
 	customerOrders := OrdersManipulation.GetOrdersByName("Russell Winfield")
-	AllOrders = AllOrders.SortBy("ascending", "total")
-	customerOrders.SortBy("descending", "total")
+	AllOrders.SortBy("descending", "date")
+	UnFulfilledOrders.SortBy("descending", "date")
+	FulfilledOrders.SortBy("descending", "date")
+	customerOrders.SortBy("descending", "date")
 
 	fmt.Println("\nAll Orders: ")
 	fmt.Println(AllOrders.GetOrderNames())
 	fmt.Println("\nCustomer name orders: ")
 	fmt.Println(customerOrders.GetOrderNames())
-	//fmt.Println("\nFulfilled Orders: ")
-	//fmt.Println(FulfilledOrders.GetOrderNames())
-	//fmt.Println("\nUnfulfilled Orders: ")
-	//fmt.Println(UnFulfilledOrders.GetOrderNames())
+	fmt.Println("\n\nFulfilled Orders: ")
+	fmt.Println(FulfilledOrders.GetOrderNames())
+	fmt.Println("\nUnfulfilled Orders: ")
+	fmt.Println(UnFulfilledOrders.GetOrderNames())
 
 }
